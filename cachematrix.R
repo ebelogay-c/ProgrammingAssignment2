dc613a621013e31b82f9d3f94c562591aeb941ef
Eugene Belogay (Coursera) <ebelogay+coursera@gmail.com>
2014-10-24 17:36:39 -0400

    Cached-inverse matrix object implemented and tested: works as required. TODO provide a tester function. TODO handle errors (not required) when: - a regular (non-cached) matrix is passed to cacheSolve(); - a singular matrix is passed to the combo.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,71 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a matrix object that caches its inverse.
 
-## Write a short comment describing this function
+## The caching is enabled by the lexical scoping mechanism in R
+## and implemented by the `<<-` operator.
+## The (possibly cached) inverse of the matrix
+## is returned by `cacheSolve`, for example:
+##
+## m = matrix(1:4, 2, 2)
+## cm = makeCacheMatrix(m)
+## inv1 = cacheSolve(cm) ## computed and cached
+## inv2 = cacheSolve(cm) ## retrieved from cache
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+    ## "private" member: cached inverse
+    cinv <- NULL
+
+    ## matrix accessors
+    get <- function() x
+    set <- function(v) {
+        x <<- v; ## save to creating enviro
+        cinv <<- NULL ## invalidate cache
+    }
+
+    ## cache accessors
+    getInv <- function() cinv
+    setInv <- function(v) cinv <<- v
+
+    ## return a named list of "object methods"
+    list(get = get,
+         set = set,
+         getInv = getInv,
+         setInv = setInv)
+
+}#makeCacheMatrix
 
 
-## Write a short comment describing this function
+## Returns the (possibly cached) inverse of a matrix.
+
+## Pre-condition:
+## The matrix must be invertible and
+## must have been created with the `makeCacheMatrix()`
+## factory method ("constructor").
+
+## The inverse is (lazily) computed only once,
+## namely, the first time it is needed.
+## After that, the cached value is returned.
+## If the inverse is never needed (cacheSolve never called),
+## it is never computed (hence the term "lazy evaluation").
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+    ## Return a matrix that is the inverse of 'x'
+
+    ## Has the inverse been cached? If yes, just return it
+#     if (exists("x$getInv") && !is.null(x$getInv())) {
+    if (!is.null(x$getInv())) {
+        message("getting cached inverse")
+        return(x$getInv())
+    }
+
+    ## If not yet cached, compute it and cache it
+    message("computing and caching inverse")
+    # compute it
+    ret <- solve(x$get())
+    # cache it
+    x$setInv(ret)
+
+    ## return it
+    return(ret)
+
+}# cacheSolve
